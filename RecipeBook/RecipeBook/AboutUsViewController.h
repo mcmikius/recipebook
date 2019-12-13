7d358596b9558df14aa2979780fd9af458101d30
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-13 11:43:19 +0200

    add webView

---
+++ RecipeBook/RecipeBook/AboutUsViewController.h
@@ -0,0 +1,18 @@
+//
+//  AboutUsViewController.h
+//  RecipeBook
+//
+//  Created by Mykhailo Bondarenko on 13.12.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AboutUsViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RecipeBook/RecipeBook/AboutUsViewController.m
@@ -0,0 +1,37 @@
+//
+//  AboutUsViewController.m
+//  RecipeBook
+//
+//  Created by Mykhailo Bondarenko on 13.12.2019.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+#import "AboutUsViewController.h"
+
+@interface AboutUsViewController ()
+
+@end
+
+@implementation AboutUsViewController
+
+@synthesize webView;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource: @"" ofType: nil]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [webView loadRequest:request];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
