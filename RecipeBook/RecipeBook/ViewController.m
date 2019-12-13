0db014487b2268d43e7735ce41829e29ddf14928
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-12-13 11:00:30 +0200

    add tableView to ViewController

--- RecipeBook/RecipeBook/ViewController.m
+++ RecipeBook/RecipeBook/ViewController.m
@@ -12,11 +12,29 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    NSArray * recipes;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    recipes = [NSArray arrayWithObjects:@"French meat", @"Salmon steak", @"Napoleon cake", @"Vegateble soup", nil];
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return recipes.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *simpleTableIdetifier = @"SimpleTableCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdetifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdetifier];
+        
+    }
+    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
+    return cell;
 }
 
 
